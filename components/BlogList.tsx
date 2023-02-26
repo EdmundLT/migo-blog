2d8e0d8bf635ac3413c5ec276dbc5cfa8b9dfde6
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2023-02-25 22:01:43 -0500

    new google manger

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -8,7 +8,8 @@ type Props = {
 function BlogList({ posts }: Props) {
   return (
     <div>
-      <hr className="border-[#0a65f7] mb-10" />
+      <div className="divider">Blog</div>
+
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16">
         {/* Posts */}
         {posts.map((post) => (
