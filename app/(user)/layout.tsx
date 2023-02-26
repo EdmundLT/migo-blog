7522bc3480e34c0b1f17e83932e06364a46a90b3
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2023-02-25 22:27:30 -0500

    advance gtm added

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -2,7 +2,6 @@ import Banner from "../../components/Banner";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import "../../styles/globals.css";
-
 export default function RootLayout({
   children,
 }: {
@@ -11,6 +10,12 @@ export default function RootLayout({
   return (
     <html>
       <body className="max-w-7xl mx-auto bg-white">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
         <Header />
         <Banner />
         {children}
