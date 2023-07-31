be627b6e02b93f8187e58e6c6c3baf1fe76b2c48
LONG TANG <ltang@LONGs-MacBook-Pro.local>
2023-07-31 01:57:17 -0400

    restore

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const withOptimizedImages = require("next-optimized-images");
-const { i18nRewriter } = require("next-i18n-router");
-const i18nConfig = require("./i18nConfig");
 
 module.exports = withOptimizedImages({});
 
 module.exports = {
-  // async rewrites() {
-  //   return {
-  //     afterFiles: i18nRewriter(i18nConfig),
-  //   };
-  // },
   async redirects() {
     return [
       {
